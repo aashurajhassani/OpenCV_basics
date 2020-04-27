1375205cbaaece9c57f6a6f3932b39caf368e0a8
Aashuraj Hassani <60579751+aashurajhassani@users.noreply.github.com>
2020-04-27 21:17:04 +0530

    Create 28 - Hough Line Transform Theory.py

---
+++ Source_1/28 - Hough Line Transform Theory.py
@@ -0,0 +1,2 @@
+# it is a technique used to detect any particular shape that can be represented in a mathematical form, even if it is distorted or open
+# see the video to understand concept properly
